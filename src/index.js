0547d34827271a45f17a45bb933bbe6c62fa192a
Cassios Marques <cassios.kmm@gmail.com>
2021-09-10 14:04:54 -0300

    Enable CORS support
    Allow calling the api on frontend
    
    Ref: https://medium.com/zero-equals-false/using-cors-in-express-cac7e29b005b

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 
 const itemController = require('./controllers/item');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
